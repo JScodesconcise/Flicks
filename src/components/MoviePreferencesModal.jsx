7ab62794fc83d1539389d935f1ef9334aa0f3822
lucastefanescu <45986033+lucastefanescu@users.noreply.github.com>
2025-02-24 20:44:37 +0000

    Started styling preference modal

--- src/components/MoviePreferencesModal.jsx
+++ src/components/MoviePreferencesModal.jsx
@@ -1,5 +1,6 @@
 import Select from "react-select"
 import genres from "../Data/genres.json"
+import "../styling/MoviePreferences.css"
 
 const mappedGenres = genres.map((name) => ({
     value: name.toLowerCase(),
@@ -9,18 +10,21 @@ const mappedGenres = genres.map((name) => ({
 function MoviePreferencesModal() {
     return(
         <div>
-            <div>
-                <h1>Set movie preferences</h1>
-                <h2>Preferred Genres</h2>
-                <Select options={mappedGenres}/>
-                <h2>Preferred Genres</h2>
-                <h2>What are Your 3 Favourite Movies</h2>
-                <Select/>
-                <h2>What is your mood right now</h2>
-                <Select/>
-                <h2>Do you Prefer Older Movies or Recent Movies</h2>
-                <Select/>
-                <button>SUBMIT</button>
+            <div className="preference-modal">
+                <div className="preference-modal-inside">
+                    <h1>Set movie preferences</h1>
+                    <h2>Preferred Genres</h2>
+                    <Select options={mappedGenres} isMulti={true}/>
+                    <h2>What are Your 3 Favourite Movies</h2>
+                    <Select/>
+                    <h2>How Much Do You Enjoy Animated Movies (1-5)</h2>
+                    <Select/>
+                    <h2>What is your mood right now</h2>
+                    <Select/>
+                    <h2>Do you Prefer Older Movies or Recent Movies</h2>
+                    <Select/>
+                    <button>SUBMIT</button>
+                </div>
             </div>
         </div>
     )
