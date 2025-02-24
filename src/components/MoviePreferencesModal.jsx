99186f8da3b1eec24bfe23cb625e8f49fadc696b
lucastefanescu <45986033+lucastefanescu@users.noreply.github.com>
2025-02-24 20:02:56 +0000

    final changes

--- src/components/MoviePreferencesModal.jsx
+++ src/components/MoviePreferencesModal.jsx
@@ -8,11 +8,21 @@ const mappedGenres = genres.map((name) => ({
 
 function MoviePreferencesModal() {
     return(
-        <>
+        <div>
             <div>
+                <h1>Set movie preferences</h1>
+                <h2>Preferred Genres</h2>
                 <Select options={mappedGenres}/>
+                <h2>Preferred Genres</h2>
+                <h2>What are Your 3 Favourite Movies</h2>
+                <Select/>
+                <h2>What is your mood right now</h2>
+                <Select/>
+                <h2>Do you Prefer Older Movies or Recent Movies</h2>
+                <Select/>
+                <button>SUBMIT</button>
             </div>
-        </>
+        </div>
     )
 }
 
