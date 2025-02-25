b7cb50574c9eadbd3b9f36d22dae221b44a5f9ca
lucastefanescu <45986033+lucastefanescu@users.noreply.github.com>
2025-02-25 22:14:25 +0000

    abc123

--- src/components/MoviePreferencesModal.jsx
+++ src/components/MoviePreferencesModal.jsx
@@ -1,6 +1,46 @@
 import Select from "react-select"
+import AsyncSelect from "react-select/async";
 import genres from "../Data/genres.json"
 import "../styling/MoviePreferences.css"
+import debounce from 'lodash/debounce';
+
+
+const API_KEY = process.env.REACT_APP_TMBD_API_KEY
+
+const fetchMovies = async(inputValue) => {
+    if(!inputValue) return [];
+
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(inputValue)}&include_adult=true&language=en-US&page=1`, options)
+        if (!response.ok) console.log("Error fetching movie")
+        const result = await response.json()
+
+        return result.results.map((movie) =>{
+            {
+                value: movie.original_title.toLowerCase()
+                label: movie.original_title
+            }
+            
+        })
+
+    }
+    catch(error){
+        console.log(error)
+        return [];
+    }
+}
+
+const loadOptions = async((inputValue) => {
+    return fetchMovies(inputValue).catch(() => []);
+}, 300);
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${API_KEY}`
+    }
+};
 
 const mappedGenres = genres.map((name) => ({
     value: name.toLowerCase(),
@@ -16,7 +56,7 @@ function MoviePreferencesModal() {
                     <h2>Preferred Genres</h2>
                     <Select options={mappedGenres} isMulti={true}/>
                     <h2>What are Your 3 Favourite Movies</h2>
-                    <Select/>
+                    <AsyncSelect loadOptions={loadOptions}/>
                     <h2>How Much Do You Enjoy Animated Movies (1-5)</h2>
                     <Select/>
                     <h2>What is your mood right now</h2>
@@ -31,3 +71,7 @@ function MoviePreferencesModal() {
 }
 
 export default MoviePreferencesModal;
+
+
+
+  
