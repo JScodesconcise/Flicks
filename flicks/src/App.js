ea0812ba12fe717fdd2e27f446feb456d8136fac
Aryan Chaudhary <98985506+Lep15@users.noreply.github.com>
2025-03-07 18:58:28 -0500

    Update App.js

--- flicks/src/App.js
+++ flicks/src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import SearchPage from "./components/SearchPage"; // Import the Search Page
 
 function App() {
-	return <></>;
+  return <SearchPage />;
 }
 
 export default App;
