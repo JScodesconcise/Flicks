3f493aff6724e545d2cca25b743da311ed1212ae
Armaanjeet Braich <armaan.b03@gmail.com>
2025-03-07 20:16:53 -0500

    finish movie info page UI

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import MoviePage from "./components/MoviePage";
+
+function App() {
+	return (
+		<MoviePage
+            title="Inception"
+            year="2010"
+            director="Christopher Nolan"
+            advisory="PG-13"
+            length="2h 28m"
+            genres={["Action", "Sci-Fi", "Thriller"]}
+            overview="A thief who enters the dreams of others to steal secrets gets a final mission."
+            rating={5}  // Out of 5 stars
+            cast={["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"]}
+            flicksRating={4.3}
+        />
+	  );
+}
+
+export default App;
