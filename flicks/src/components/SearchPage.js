2b673e39f070c882227d5da13d7e5671eccf7b96
Aryan Chaudhary <98985506+Lep15@users.noreply.github.com>
2025-03-07 19:29:39 -0500

    Update SearchPage.js

--- flicks/src/components/SearchPage.js
+++ flicks/src/components/SearchPage.js
@@ -1,34 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../styling/SearchPage.css";
-import MovieCard from "./MovieCard";
-import Navbar from "./Navbar"; // Import Navbar
+import Navbar from "./Navbar"; // Navbar is already added
 
 function SearchPage() {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    // Simulate fetching from a database
-    fetch("movies.json")
-      .then((res) => res.json())
-      .then((data) => setMovies(data))
-      .catch((err) => console.error("Error loading movies:", err));
-  }, []);
-
   return (
     <div className="search-page">
-      <Navbar /> {/* Navbar Added Here */}
-      
+      <Navbar /> {/* Navbar stays at the top */}
+
+      {/* Search Bar */}
       <div className="search-container">
-        <input type="text" className="search-box" placeholder="Search for your favorite movies..." />
+        <input type="text" className="search-box" placeholder="Search" />
       </div>
 
-      <h2 className="section-title">SEARCH FOR YOUR FAVORITE MOVIES</h2>
-
-      <div className="movie-grid">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} title={movie.title} image={movie.image} />
-        ))}
-      </div>
+      {/* Section Title */}
+      <h2 className="section-title">Search for your favorite movies!</h2>
     </div>
   );
 }
