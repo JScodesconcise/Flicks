abda4c3184e293cbf1636d27e0e7994f6cc3d470
lucastefanescu <lucastefanescu383@gmail.com>
2025-03-09 17:36:44 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import FlicksLandingPage from "./components/MainPage.js"
+import FlicksLandingPage from "./components/MainPage.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-	return (<FlicksLandingPage/>
-	);
+	return <></>;
 }
 
 export default App;
---
+++ src/components/MainPageNavbar.jsx
@@ -0,0 +1,30 @@
+import "../styling/MainPageNavbar.css";
+import logo from "../styling/flick_logo.png";
+
+function MainPageNavBar({ handleLoggedInCheck, handleSignup, handleLogin }) {
+	return (
+		<>
+			<nav className="nav-container">
+				<div className="logo-container">
+					<img src={logo} alt="flicks logo"></img>
+				</div>
+				<div className="nav-buttons">
+					<button
+						className="nav-button home-button"
+						onClick={() => handleLoggedInCheck("profile")}
+					>
+						Profile
+					</button>
+					<button className="nav-button about-button" onClick={handleSignup}>
+						Sign Up
+					</button>
+					<button className="nav-button login-button" onClick={handleLogin}>
+						Login
+					</button>
+				</div>
+			</nav>
+		</>
+	);
+}
+
+export default MainPageNavBar;
