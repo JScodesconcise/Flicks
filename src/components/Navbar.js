68a4f010cc4e99abfad4d52326ee89536b60cbd6
lucastefanescu <45986033+lucastefanescu@users.noreply.github.com>
2025-03-12 00:07:24 +0000

    re-did css called the tmdb api to show popular movies

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+// import React from "react";
+// import "../styling/Navbar.css";
+
+// function Navbar() {
+//   return (
+//     <nav className="navbar">
+//       {/* Movie Logo (Top Left) */}
+//       <div className="logo">
+//         <img src="/assets/flick logo 2.svg" alt="Movie Logo" className="movie-logo" />
+//       </div>
+
+//       {/* Navigation Buttons */}
+//       <div className="nav-links">
+//         <button>Profile</button>
+//         <button>Sign In</button>
+//         <button>Register</button>
+//         <button>Search</button>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
