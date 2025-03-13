f17af4be774f9c3fb9b5dc4ed10e6a5f34c28798
Armaanjeet Braich <armaan.b03@gmail.com>
2025-03-13 01:44:51 -0400

    Add API connection to fetch movie data based on title and year

--- src/App.js
+++ src/App.js
@@ -5,17 +5,9 @@ import MoviePage from "./components/MoviePage";
 function App() {
 	return (
 		<MoviePage
-            title="Inception"
-            year="2010"
-            director="Christopher Nolan"
-            advisory="PG-13"
-            length="2h 28m"
-            genres={["Action", "Sci-Fi", "Thriller"]}
-            overview="A thief who enters the dreams of others to steal secrets gets a final mission."
-            rating={5} 
-            image="https://i.ebayimg.com/00/s/MTYwMFgxMDk3/z/LlUAAOSwm8VUwoRL/$_57.JPG?set_id=880000500F"
-            cast={["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"]}
-            flicksRating={4.3}
+            movieName="mulholland drive"
+            year="2002"
+           
         />
 	  );
 }
